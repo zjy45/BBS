46e314a2a510fd0e793614317ec4f60953851109
zjy45 <zhujiayu@zhujiayudeiMac.local>
2017-09-08 00:53:50 +0800

    删减无用注释

--- models/__init__.py
+++ models/__init__.py
@@ -21,45 +21,25 @@ def load(path):
         return json.loads(s)
 
 
-# Model 是一个 ORM（object relation mapper）
-# 好处就是不需要关心存储数据的细节，直接使用即可
 class Model(object):
-    """
-    Model 是所有 model 的基类
-    @classmethod 是一个套路用法
-    例如
-    user = User()
-    user.db_path() 返回 User.txt
-    """
     @classmethod
     def db_path(cls):
-        """
-        cls 是类名, 谁调用的类名就是谁的
-        classmethod 有一个参数是 class(这里我们用 cls 这个名字)
-        所以我们可以得到 class 的名字
-        """
         classname = cls.__name__
         path = 'data/{}.txt'.format(classname)
         return path
 
     @classmethod
     def _new_from_dict(cls, d):
-        # 因为子元素的 __init__ 需要一个 form 参数
-        # 所以这个给一个空字典
+        # 因为子元素的 __init__ 需要一个 form 参数,所以这个给一个空字典
         m = cls({})
         for k, v in d.items():
-            # setattr 是一个特殊的函数
-            # 假设 k v 分别是 'name'  'gua'
-            # 它相当于 m.name = 'gua'
             setattr(m, k, v)
         return m
 
     @classmethod
     def new(cls, form, **kwargs):
         m = cls(form)
-        # 额外地设置 m 的属性
         for k, v in kwargs.items():
-            # 这是一个神奇的函数, 可以设置对象的属性
             setattr(m, k, v)
         m.save()
         return m
@@ -71,9 +51,6 @@ class Model(object):
         """
         path = cls.db_path()
         models = load(path)
-        # 这里用了列表推导生成一个包含所有 实例 的 list
-        # 因为这里是从 存储的数据文件 中加载所有的数据
-        # 所以用 _new_from_dict 这个特殊的函数来初始化一个数据
         ms = [cls._new_from_dict(m) for m in models]
         return ms
 
@@ -86,7 +63,6 @@ class Model(object):
             k, v = key, value
         all = cls.all()
         for m in all:
-            # 也可以用 getattr(m, k) 取值
             if v == m.__dict__[k]:
                 ms.append(m)
         return ms
@@ -103,7 +79,6 @@ class Model(object):
             k, v = key, value
         all = cls.all()
         for m in all:
-            # 也可以用 getattr(m, k) 取值
             if v == m.__dict__[k]:
                 return m
         return None
@@ -137,11 +112,6 @@ class Model(object):
             return obj
 
     def __repr__(self):
-        """
-        __repr__ 是一个魔法方法
-        简单来说, 它的作用是得到类的 字符串表达 形式
-        比如 print(u) 实际上是 print(u.__repr__())
-        """
         classname = self.__class__.__name__
         properties = ['{}: ({})'.format(k, v) for k, v in self.__dict__.items()]
         s = '\n'.join(properties)
@@ -151,7 +121,6 @@ class Model(object):
         """
         返回当前 model 的字典表示
         """
-        # copy 会复制一份新数据并返回
         d = self.__dict__.copy()
         return d
 
@@ -163,12 +132,10 @@ class Model(object):
         # log('debug save')
         models = self.all()
         # log('models', models)
-        # 如果没有 id，说明是新添加的元素
         if self.id is None:
             # 设置 self.id
             # 先看看是否是空 list
             if len(models) == 0:
-                # 我们让第一个元素的 id 为 1（当然也可以为 0）
                 self.id = 1
             else:
                 m = models[-1]
